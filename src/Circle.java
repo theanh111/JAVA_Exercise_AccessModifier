351b18c06107269bbccb04566ba9daeb3d91e1f5
anh.tran(c0720i1) <69283798+theanh111@users.noreply.github.com>
2020-09-16 08:02:01 +0700

    test cac Pham vi truy cap

--- src/Circle.java
+++ src/Circle.java
@@ -2,18 +2,18 @@ public class Circle {
     private double radius = 1.0;
     private String color = "red";
 
-    public Circle(){
+    protected Circle(){
     }
 
-    public Circle(double radius) {
+    protected Circle(double radius) {
         this.radius = radius;
     }
 
-    public double getRadius() {
+    protected double getRadius() {
         return this.radius;
     }
 
-    public double getArea() {
+    protected double getArea() {
         return 2 * this.radius * Math.PI;
     }
 }
--- src/TestCircle.java
+++ src/TestCircle.java
@@ -1,6 +1,6 @@
 public class TestCircle {
     public static void main(String[] args) {
-        Circle circle = new Circle();
+        Circle circle = new Circle(2.0);
         System.out.println("Radius is: " + circle.getRadius());
         System.out.println("Area is: " + circle.getArea());
     }
